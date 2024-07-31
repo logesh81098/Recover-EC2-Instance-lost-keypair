37e47898456279c5b51967df57d590a8cacf383d
Logesh Shanmugavel <logesh81098@gmail.com>
2024-08-01 02:01:48 +0530

    Instance got terminated if I use terraform apply command and found that instance is mutable now and noticed that I need to vpc_security_groups instead of security_groups in Linux server module to make instance as immutable

--- module/Linux-instance/main.tf
+++ module/Linux-instance/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "original-instance" {
   ami = var.ami-id
   instance_type = var.instance-type
   subnet_id = var.subnet-id
-  security_groups = [var.security-group]
+  vpc_security_group_ids = [var.security-group]
   key_name = var.key-pair
   associate_public_ip_address = true
   tags = {
